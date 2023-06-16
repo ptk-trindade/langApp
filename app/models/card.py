45dfb5fb284af83df7d58f9ac2d4b224c6c86444
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2023-06-16 18:33:42 -0300

    Removed old Card class instance

--- app/models/card.py
+++ app/models/card.py
@@ -6,14 +6,15 @@ import card.db as card_db
 
 class Card:
     
-    def __init__(self, card_id: int, concepts = [], hasAudio = False, front = '', back = ''):
+    def __init__(self, card_id: int, concepts = [], hasAudio = False, front = '', back = '', sourceLanguage_id = 0, targetLanguage_id = 0, creator_id = 0):
         self.id = card_id
         self.concepts = concepts
         self.hasAudio = hasAudio
         self.front = front
         self.back = back
-        self.sourceLanguage = ''
-        self.targetLanguage = ''
+        self.sourceLanguage_id = sourceLanguage_id
+        self.targetLanguage_id = targetLanguage_id
+        self.creator_id = creator_id
 
 
     def toJson(self):
